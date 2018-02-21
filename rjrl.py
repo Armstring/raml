b15ce0f9f51d24d1a239468dd53b185304f18b03
Ruitong Huang <rtonghuang@gmail.com>
2018-02-21 14:39:46 -0500

    working version 1.0

--- rjrl.py
+++ rjrl.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from torch.autograd import Variable
 import torch.optim as optim
 import torch.nn.functional as F
+import torch.nn.init as init
 
 import gym
 import random
@@ -18,47 +19,56 @@ num_char = env.observation_space.n #6
 dim_obs = num_char # 0,1,2,3,4,5
 dim_output = 4* (num_char-1) # ({0,1}, {0,1}, {0,1,2,3,4})
 dim_input = dim_output +  dim_obs 
-dim_hidden = 256
+dim_hidden = 64
 tau = 0.1
+online = False
 
 #################
 num_epoch = 4000
-num_episode = 40
-num_trial = 10
+num_episode = 10
+num_trial = 40
 batch_size = num_episode*num_trial
 MAX_STEP = 1e5
-num_iter = 150
 clip_norm = 50.0
-adam_reset = True
-test_size = 100
-lr = 0.01
-decay_rate = 0.9999
+adam_reset = False
+test_size = batch_size
+lr = 0.005
+decay_rate = 1.0
 
 ############################
 h0 = torch.zeros(dim_hidden).view(1,-1)
 c0 = torch.zeros(dim_hidden).view(1,-1)
-p0 = 1.0/ dim_output * torch.ones(dim_output).view(1,-1)
 
+#seed_list = []
+#for ind in range(batch_size):
+#	seed_int = random.randrange(10000000)
+#	seed_list.append(seed_int)
 #####################	
 def weights_init(layer):
 	classname = layer.__class__.__name__
-	if classname.find('Conv') != -1:
-		layer.weight.data.normal_(0.0, 0.02)
-	elif classname.find('Lin') != -1:
-		layer.weight.data.normal_(0.0, 0.02)
+	if 'LSTM' in classname:
+		for parameter in layer.parameters():
+			parameter.data.normal_(0.0, 0.2)
+			parameter.data.clamp_(-2.0,2.0)
+	elif 'Lin' in classname:
+		layer.weight.data.normal_(0.0, 0.2)
+		layer.weight.data.clamp_(-2.0,2.0)
 
 def weights_xavier_init(layer):
 	classname = layer.__class__.__name__
-	if classname.find('Conv') != -1:
-		layer.weight.data.normal_(0.0, 0.02)
-	elif classname.find('Lin') != -1:
+	if 'LSTM' in classname:
+		for parameter in layer.parameters():
+			init.xavier_uniform(parameter.data)
+			parameter.data.clamp_(-2.0,2.0)
+	elif 'Lin' in classname:
 		init.xavier_uniform(layer.weight.data)
+		layer.weight.data.clamp_(-2.0,2.0)
 
 class netP(nn.Module):
 	def __init__(self):
 		super(netP, self).__init__()
-		self.fc1 = nn.Linear(dim_input, dim_input)
-		self.lstm = nn.LSTMCell(dim_input, dim_hidden)
+		self.fc1 = nn.Linear(dim_input, dim_hidden)
+		self.lstm = nn.LSTMCell(dim_hidden, dim_hidden)
 		self.fc2 = nn.Linear(dim_hidden, dim_output)
 		self.apply(weights_init)
 	def forward(self, input, h, c):
@@ -67,19 +77,31 @@ class netP(nn.Module):
 		output = F.softmax(self.fc2(h))
 		return output, h, c
 
-def onehot(ind):
+def onehot_obs(ind):
 	res = torch.zeros(1,dim_obs)
 	res[0][ind] = 1.0
-	res = Variable(res, requires_grad = False)
 	if use_cuda:
-		return res.cuda()
-	else:
-		return res
+		res =  res.cuda()
+	res = Variable(res, requires_grad = False)
+	return res
+
+def onehot_action(ind):
+	res = torch.zeros(1,dim_output)
+	if ind>=0:
+		res[0][ind] = 1.0
+	if use_cuda:
+		res =  res.cuda()
+	res = Variable(res, requires_grad = False)
+	return res
+
+def save_log(var):#
+	return torch.log(torch.clamp(var,min=1e-10))
 
 def softmax(ll):
 	ll = np.asarray(ll)
 	ll = np.exp(ll - ll.max())
-	return ll/np.sum(ll)
+	ll = 1.0*ll/np.sum(ll)
+	return ll
 
 def action_id_to_action(ind):
 	move = ind//((num_char-1)*2) 
@@ -88,67 +110,89 @@ def action_id_to_action(ind):
 	content = remain% (num_char-1) #mod 5
 	return (move, write, content)
 
-def generate_batch(samplerNet, num_episode, number_trial):
+def generate_batch(samplerNet, num_episode, num_trial):
 	actions_batch = []
 	rewards_batch = []
 	obs_batch = []
+	seed_list = []
 	for ind in range(num_trial):
 		seed_int = random.randrange(10000000)
+		
 		for episode in range(num_episode):
+			seed_list.append(seed_int)
+			seed_int = seed_list[ind*num_episode + episode]
 			env.seed(seed_int)
 			obs = env.reset()
 			FLAG = False
 			
 			h = Variable(h0)
 			c = Variable(c0)
-			output = Variable(p0)
-			net_input = torch.cat(( output, onehot(obs)), dim=1)
+			action=-1
 
 			action_list = []
 			reward_list = []
 			obs_list = []
 			while not FLAG:
+				net_input = torch.cat(( onehot_action(action), onehot_obs(obs)), dim=1)
 				obs_list.append(obs)
 				output, h,c = samplerNet(net_input, h, c)
 				action = int(torch.distributions.Categorical(output).sample().data.cpu())
 				action_list.append(action)
-				action = action_id_to_action(action)
-				obs, reward, FLAG, info = env.step(action)
+				action_tuple = action_id_to_action(action)
+				obs, reward, FLAG, info = env.step(action_tuple)
 				reward_list.append(reward)
-				net_input = torch.cat((output, onehot(obs)), dim=1)
 
 			actions_batch.append(action_list)
 			rewards_batch.append(reward_list)
 			obs_batch.append(obs_list)
-	return actions_batch, rewards_batch, obs_batch
+	return actions_batch, rewards_batch, obs_batch,seed_list
 	
 def test_reward(net, size):
 	for ind in range(size):
+		#seed_int = seed_list[ind]
+		#env.seed(seed_int)
 		obs = env.reset()
 		h = Variable(h0)
 		c = Variable(c0)
-		output = Variable(p0)
 		reward_list = []
+		loss_list = []
 		FLAG = False
-
+		action = -1
 		while not FLAG:
-			net_input = torch.cat(( output, onehot(obs)), dim=1)
+			net_input = torch.cat(( onehot_action(action), onehot_obs(obs)), dim=1)
 			output, h,c = net(net_input, h, c)
 			action = int(torch.distributions.Categorical(output).sample().data.cpu())
-			action = action_id_to_action(action)
-			obs, reward, FLAG, info = env.step(action)
+			action_tuple = action_id_to_action(action)
+			obs, reward, FLAG, info = env.step(action_tuple)
 			reward_list.append(reward)
+			loss_list.append(save_log(output[0][action]).data.cpu().numpy())
+	return np.mean(reward_list), -1.0*np.mean(loss_list)
+'''
+def test_loss(net, obs_batch, actions_batch, weight_list): #for debug
+	loss = 0.0
+	for ind in range(batch_size):
+		weight = weight_list[ind]
+		action_list = actions_batch[ind]
+		obs_list = obs_batch[ind]
 
-	return np.mean(reward_list)
+		h = Variable(h0)
+		c = Variable(c0)
+		action = -1
 
-def construct_w(rewards_batch, num_episode, num_trial):
-	weight_list = np.ndarray([])
+		for y, obs in zip(action_list, obs_list):
+			net_input = torch.cat(( onehot_action(action), onehot_obs(obs)), dim=1)
+			output, h, c = net(net_input, h, c)
+			loss += -1.0* weight.item()*save_log(output[0][y]).data
+			action = y
+	return loss/batch_size
+'''
+def construct_w(weight_list, num_episode, num_trial):
+	res = np.ndarray(0)
 	for ind in range(num_trial):
-		temp = rewards_batch[ind*num_episode: (ind+1)*num_episode]
-		temp_weight = [1.0*np.sum(reward_list)/tau for reward_list in temp ]
-		temp_weight = softmax(temp_weight)
-		weight_list = np.append(weight_list, temp_weight)
-	return weight_list
+		temp = weight_list[(ind*num_episode):((ind+1)*num_episode)]
+		temp_weight = softmax(temp)
+		res = np.append(res, temp_weight)
+	return res
 ###############################################################################
 ###############################################################################
 policyNet = netP()
@@ -161,40 +205,41 @@ if use_cuda:
 	policyNet_sample = policyNet_sample.cuda()
 	h0 = h0.cuda()
 	c0 = c0.cuda()
-	p0 = p0.cuda()
 
 num_step = 0
-for epoch in range(num_epoch):
+for epoch in range(1, num_epoch+1):
 	policyNet_sample.load_state_dict(policyNet.state_dict())
 	pre_loss = -100.0
 
-	if adam_reset and epoch % 40==0:
+	if adam_reset and num_step % 2000==0:
 		q = optimizer.param_groups[0]['lr']
 		optimizer = optim.Adam(policyNet.parameters(), lr=q)
-
-	for iter in range(3*epoch):
-		actions_batch, rewards_batch, obs_batch = generate_batch(policyNet_sample, num_episode, num_trial)
-		weight_list = construct_w(rewards_batch, num_episode, num_trial)
-		#weight_sum_list = [1.0*np.sum(reward_list)/tau for reward_list in rewards_batch ]
-		#weight_list = softmax(weight_sum_list)
+	
+		
+	for iter in range(10*epoch): #while True:	#
+		if online:
+			actions_batch, rewards_batch, obs_batch, seed_list = generate_batch(policyNet, num_episode, num_trial)
+		else:
+			actions_batch, rewards_batch, obs_batch, seed_list = generate_batch(policyNet_sample, num_episode, num_trial)
+		weight_sum_list = [1.0*np.sum(reward_list)/tau for reward_list in rewards_batch ]
+		weight_list = construct_w(weight_sum_list, num_episode, num_trial)
 		loss_list = []
 		
 		policyNet.zero_grad()
 		for ind in range(batch_size):
 			weight = weight_list[ind]
-			#seed_int = seeds_batch[ind]
 			action_list = actions_batch[ind]
 			obs_list = obs_batch[ind]
 
 			h = Variable(h0)
 			c = Variable(c0)
-			output = Variable(p0)
-			for action, obs in zip(action_list, obs_list):
-				net_input = torch.cat(( output, onehot(obs)), dim=1)
+			action = -1
+
+			for y, obs in zip(action_list, obs_list):
+				net_input = torch.cat(( onehot_action(action), onehot_obs(obs)), dim=1)
 				output, h, c = policyNet(net_input, h, c)
-
-				loss_list.append(-1.0* weight.item()* torch.log(output[0][action])) #-log(\pi)
-
+				loss_list.append(-1.0* weight.item()* save_log(output[0][y])) #-log(\pi)
+				action = y
 
 		loss = sum(loss_list)/batch_size
 		loss.backward()
@@ -202,15 +247,15 @@ for epoch in range(num_epoch):
 		optimizer.step()
 		optimizer.param_groups[0]['lr'] *= decay_rate
 		
-		#print((pre_loss, loss.data.cpu()[0]))
-		if abs(pre_loss - loss.data.cpu()[0]) < 1e-5:
+		if abs(pre_loss - loss.data.cpu()[0]) < 1e-6:
 			break
 		else:
 			pre_loss = loss.data.cpu()[0]
 			
 		if num_step % 50==0:
-			reward_test = test_reward(policyNet, test_size)
-			print("[Reset: %r; Epoch: %d; num_step: %d] Pre loss: %.5f; Test reward: %.3f" %(adam_reset, epoch, num_step, pre_loss, reward_test))
+			#loss_test = 0# test_loss(policyNet, obs_batch, actions_batch, weight_list)
+			reward_test , loss_test= test_reward(policyNet, test_size)
+			print("[Reset: %r; Epoch: %d; num_step: %d] Train loss: %.5f; Test loss: %.3f; Test reward: %.3f" %(adam_reset, epoch, num_step, pre_loss, loss_test, reward_test))
 		num_step +=1
 	if num_step > MAX_STEP:
 		break
